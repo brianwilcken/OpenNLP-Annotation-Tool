a99b8b99746d47e698f6bb1e2adb87d669916036
Brian Wilcken <brian.wilcken@inl.gov>
2019-06-14 16:11:53 -0600

    fix bug in entropy percentage cutoff

--- src/main/java/nlpannotator/TrainingIterations.java
+++ src/main/java/nlpannotator/TrainingIterations.java
@@ -13,6 +13,7 @@ public class TrainingIterations extends JFrame {
     private JButton startButton;
     private JPanel mainPanel;
     private JSlider percentEntropySlider;
+    private JSpinner percentEntropySpinner;
     private Main main;
 
     public TrainingIterations(Main main) {
@@ -24,14 +25,18 @@ public class TrainingIterations extends JFrame {
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         pack();
 
-        initSpinner();
+        initSpinners();
         addEventHandlers();
     }
 
-    private void initSpinner() {
-        SpinnerNumberModel model = new SpinnerNumberModel(100, 10, 1000, 10);
-        iterationsSpinner.setModel(model);
+    private void initSpinners() {
+        SpinnerNumberModel iterationsModel = new SpinnerNumberModel(100, 10, 1000, 10);
+        iterationsSpinner.setModel(iterationsModel);
         iterationsSpinner.setEditor(new JSpinner.NumberEditor(iterationsSpinner));
+
+        SpinnerNumberModel percentEntropyModel = new SpinnerNumberModel(25, 0, 50, 1);
+        percentEntropySpinner.setModel(percentEntropyModel);
+        percentEntropySpinner.setEditor(new JSpinner.NumberEditor(percentEntropySpinner));
     }
 
     private void addEventHandlers() {
@@ -39,7 +44,7 @@ public class TrainingIterations extends JFrame {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 int iterations = Integer.parseInt(iterationsSpinner.getValue().toString());
-                double percentEntropy = (double) percentEntropySlider.getValue() / (double)100;
+                double percentEntropy = (double) Integer.parseInt(percentEntropySpinner.getValue().toString()) / (double) 100;
                 main.trainDoccatModel(iterations, percentEntropy);
                 setVisible(false);
             }
@@ -62,7 +67,7 @@ public class TrainingIterations extends JFrame {
      */
     private void $$$setupUI$$$() {
         mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayoutManager(2, 2, new Insets(5, 5, 5, 5), -1, -1));
+        mainPanel.setLayout(new GridLayoutManager(3, 2, new Insets(5, 5, 5, 5), -1, -1));
         final JLabel label1 = new JLabel();
         label1.setText("Enter Number of Iterations:");
         mainPanel.add(label1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
@@ -70,7 +75,12 @@ public class TrainingIterations extends JFrame {
         mainPanel.add(iterationsSpinner, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         startButton = new JButton();
         startButton.setText("Start");
-        mainPanel.add(startButton, new GridConstraints(1, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, new Dimension(300, -1), new Dimension(300, -1), new Dimension(300, -1), 0, false));
+        mainPanel.add(startButton, new GridConstraints(2, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, new Dimension(300, -1), new Dimension(300, -1), new Dimension(300, -1), 0, false));
+        final JLabel label2 = new JLabel();
+        label2.setText("Enter Entropy Percentage:");
+        mainPanel.add(label2, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        percentEntropySpinner = new JSpinner();
+        mainPanel.add(percentEntropySpinner, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
     }
 
     /**
